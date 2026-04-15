db319a2db7a53081847b2db7814de51905fdcf1f
k <k@yuanjiaban.localdomain>
2026-04-15 10:19:26 +0800

    忽略 Catkin 编译产物

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/w/clb_ws/src
-# Build directory: /home/w/clb_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("R1")
--- build/R1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/w/clb_ws/src/R1
-# Build directory: /home/w/clb_ws/build/R1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
